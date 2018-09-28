9ac6a80426ebe913daa633fdf2591edd541f6d77
TomoyaIshida <tmyisd@icloud.com>
2018-09-28 15:09:17 +0900

    Add Oracle

---
+++ ActionAndControl/Oracle/Oracle.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.24;
+
+contract Oracle {
+
+    address knownSource = 0x123...; // known source
+
+    struct Request {
+        bytes data;
+        function(bytes memory) external callback;
+    }
+
+    Request[] requests;
+
+    event NewRequest(uint);
+
+    modifier onlyBy(address _account) {
+        require(msg.sender == _account);  _;
+    }
+
+    function query(bytes _data, function(bytes memory) external _callback) public {
+        requests.push(Request(_data, _callback));
+        NewRequest(requests.length - 1);
+    }
+
+    // invoked by outside world
+    function reply(uint _requestID, bytes _response) public onlyBy(knownSource) {
+        requests[_requestID].callback(response);
+    }
+}
