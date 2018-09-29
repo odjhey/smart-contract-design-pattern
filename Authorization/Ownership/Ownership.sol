d0a3412ca2506093c26233d6cd09cf355c338283
TomoyaIshida <tmyisd@icloud.com>
2018-09-29 22:43:19 +0900

    Add Ownership pattern

---
+++ Authorization/Ownership/Ownership.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.24;
+
+contract Owned {
+    address public owner;
+
+    event LogOwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function Owned() public {
+        owner = msg.sender;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0));
+        LogOwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
+}
