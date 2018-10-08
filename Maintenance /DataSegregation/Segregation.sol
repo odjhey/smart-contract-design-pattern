0ba3dee2adf127a68cf5679ab089882bdeca8abd
TomoyaIshida <tmyisd@icloud.com>
2018-10-08 13:33:17 +0900

    Add Register

--- Maintenance /DataSegregation/Segregation.sol
+++ Maintenance /DataSegregation/Segregation.sol
@@ -1,14 +1,14 @@
 pragma solidity ^0.4.24;
 
 contract DataStorage {
-  mapping(bytes32 => uint) uintStorage;
+    mapping(bytes32 => uint) uintStorage;
 
-  function getUintValue(bytes32 key) public constant
-    returns (uint) {
-    return uintStorage[key];
-  }
+    function getUintValue(bytes32 key) public constant
+        returns (uint) {
+        return uintStorage[key];
+    }
 
-  function setUintValue(bytes32 key, uint value) public {
-    uintStorage[key] = value;
-  }
+    function setUintValue(bytes32 key, uint value) public {
+        uintStorage[key] = value;
+    }
 }
